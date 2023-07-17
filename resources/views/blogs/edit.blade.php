7ee3c8010d40290b5ebc7644f0a0bbc5af82be41
rosanno <rosannomacabuhay@gmail.com>
2023-07-17 09:14:00 +0800

    change edit page title

--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -2,7 +2,7 @@
     <section class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Create a blog') }}
+                {{ __('Update a blog') }}
             </h2>
         </x-slot>
 
